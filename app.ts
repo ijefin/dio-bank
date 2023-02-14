310e7cc973f124917168c3cf28b486cc95a2dc6f
ijefin <jeffersonmecmiler@hotmail.com>
2023-02-14 12:20:11 -0300

    fixed withdraw bug

--- app.ts
+++ app.ts
@@ -4,11 +4,8 @@ import { PeopleAccount } from "./class/PeopleAccount";
 const contaJefferson: PeopleAccount = new PeopleAccount(1, "Nath", 10);
 contaJefferson.deposit(200);
 contaJefferson.getBalance();
-contaJefferson.deposit(200);
-contaJefferson.getBalance();
-contaJefferson.withdraw(400);
-contaJefferson.getBalance();
-contaJefferson.withdraw(400);
+contaJefferson.withdraw(150);
+contaJefferson.withdraw(50);
 
 const contaJtech: CompanyAccount = new CompanyAccount("Jtech", 79437);
 console.log(contaJtech);
@@ -19,4 +16,8 @@ contaJtech.getLoan(5000);
 contaJtech.getLoan(5000);
 contaJtech.getBalance();
 contaJtech.getLoan(5000);
+contaJtech.getBalance();
+
+contaJtech.withdraw(20000)
+contaJtech.deposit(1000000)
 contaJtech.getBalance();
